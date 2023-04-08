746640a24b6d86f85964bbb2518618668af821d1
ismail-kt <kt.ismail.online@gmail.com>
2023-04-08 18:14:17 +0530

    added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\employee;
+use App\Models\task;
+
 
 class HomeController extends Controller
 {
@@ -24,5 +27,156 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users|max:255',
+            'department' => 'required',
+            'mobile' => 'required|number|max:10',
+            'status' => 'required',
+        ]);
+
+            $employee = new employee([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'department' => $request->input('department'),
+            'mobile' => $request->input('mobile'),
+            'status' => $request->input('status'),
+        ]);
+
+        $employee->save();
+
+        //return to view 
+    }
+
+    public function editEmployee($id)
+    {
+        $task = employee::find($id);
+        //return employee data to edit view
+    }
+
+    public function updateEmployee(Request $request)
+    {
+        $request->validate([
+            'id' =>'required',
+            'name' => 'required|string|max:255',
+            'department' => 'required',
+            'mobile' => 'required|number|max:10',
+            'status' => 'required',
+        ]);
+
+        $employee = employee::find($request->id);
+        $employee->name = $request->input('name');
+        $employee->department = $request->input('department');
+        $employee->mobile = $request->input('mobile');
+        $employee->status = $request->input('status');
+        $employee->save();
+
+        //return to view 
+    }
+
+    public function viewEployees()
+    {
+        // getting all employees
+        $employee = employee::all();
+        //result passing to view
+    }
+
+    public function storeTasks(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|email|unique:users|max:255',
+            // 'employee_id' => 'required|number',
+            'status' => 'required',
+        ]);
+
+            $task = new task([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'employee_id' => $request->input('employee_id'),
+            'status' => $request->input('status'),
+        ]);
+
+        $task->save();
+       //return to view
+    } 
+
+    public function viewTasks()
+    {
+        // getting all employees
+        $employee = task::all();
+        // result passing to view
+    }
+
+    public function editTask($id)
+    {
+        $task = task::find($id);
+        //return task data to edit view
+    }
+
+    public function updateTasks(Request $request)
+    {
+        $request->validate([
+            'id' => 'required',
+            'title' => 'required|string|max:255',
+            'description' => 'required|email|unique:users|max:255',
+            'status' => 'required',
+        ]);
+
+        $task = task::find($request->id);
+        $task->title = $request->input('title');
+        $task->description = $request->input('description');
+        $task->employee_id = $request->input('employee_id');
+        $task->status = $request->input('status');
+        $task->save();
+
+        //return to view 
+    }
+
+    public function assignTask(Request $request)
+    {
+        $task = task::find($request->id);
+        $task->employee_id = $request->input('employee_id');
+        $task->status = 'assigned';
+        $task->save();
+    }
+
+
+    public function assigneeChange(Request $request)
+    {
+        $task = task::find($request->id);
+        if ($task->status=='assigned') {
+            $task->employee_id = $request->input('employee_id');
+            $task->save();
+        }else{
+            //return error 'task is in progress'
+        }
+        //return success message and view
+    }
+
+    public function startTask(Request $request){
+        $currentDateTime = now()->format('Y-m-d H:i:s');
+        $task = task::find($request->id);
+        $task->status = 'inprogress';
+        $task->started = $currentDateTime;
+        $task->save();
+    }
+
+    public function completeTask(Request $request){
+        $currentDateTime = now()->format('Y-m-d H:i:s');
+        $task = task::find($request->id);
+        $startedTime = $task->started;
+        $timeDifferenceInMinutes = $startedTime->diffInMinutes($currentDateTime);
+        if (($task->status=='inprogress')&&($timeDifferenceInMinutes > 5)) {
+            $task->status = 'done';
+            $task->save();
+        }else{
+            //return error 'please submit the task after 5 minutes'
+        }
+        //return success
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,21 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/show/employees', [App\Http\Controllers\HomeController::class, 'viewEployees'])->name('employees.show');
+Route::get('/edit/employees/{$id}', [App\Http\Controllers\HomeController::class, 'editEmployee'])->name('employees.edit');
+Route::post('/update/employees', [App\Http\Controllers\HomeController::class, 'updateEmployee'])->name('employees.update');
+Route::post('/crate/employee', [App\Http\Controllers\HomeController::class, 'store'])->name('employee.store');
+
+Route::get('/show/tasks', [App\Http\Controllers\HomeController::class, 'viewTasks'])->name('task.store');
+Route::get('/edit/task/{$id}', [App\Http\Controllers\HomeController::class, 'editTask'])->name('task.edit');
+Route::post('/update/task', [App\Http\Controllers\HomeController::class, 'updateTasks'])->name('task.update');
+Route::post('/crate/task', [App\Http\Controllers\HomeController::class, 'storeTasks'])->name('task.store');
+
+Route::post('/assign/task', [App\Http\Controllers\HomeController::class, 'assignTask'])->name('task.assign');
+Route::post('/reassign/task', [App\Http\Controllers\HomeController::class, 'assigneeChange'])->name('task.assign.change');
+Route::post('/start/task', [App\Http\Controllers\HomeController::class, 'startTask'])->name('task.start');
+Route::post('/complete/task', [App\Http\Controllers\HomeController::class, 'completeTask'])->name('task.complete');
+
+
+
